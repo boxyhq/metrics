898b6fe596a7317964d15f44db38157964ea25b6
Deepak Prabhakara <deepak@boxyhq.com>
2024-08-15 12:10:45 +0530

    migrated config

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+import { includeIgnoreFile } from '@eslint/compat';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+
+export default [
+  {
+    ignores: ['.prettierrc.js'],
+  },
+  includeIgnoreFile(gitignorePath),
+  ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+    },
+
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+
+    files: ['**/*.ts'],
+  },
+];
